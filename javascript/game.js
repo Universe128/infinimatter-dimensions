28edd23217a8856705c7feee27a28fdbff0e557c
Matthew Green <40971545+Universe128@users.noreply.github.com>
2020-03-23 12:32:29 +1100

    Create game.js

---
+++ javascript/game.js
@@ -0,0 +1,6 @@
+//test
+var TIER_NAMES = [ null, "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eight" ];
+var DISPLAY_NAMES = [ null, "First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth" ];
+var player = {
+  money: new Decimal(10)
+  }
